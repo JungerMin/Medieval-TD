808d6009e48d329cb9224f75748d79e43fff0f28
JungerMin <jungermin.github@gmail.com>
2021-04-14 10:22:03 +0200

    Added Pausemenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject confirm;
+    public GameObject exitLevel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            confirm.SetActive(false);
+            exitLevel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void ExitLevel()
+    {
+        confirm.SetActive(true);
+        exitLevel.SetActive(false);
+    }
+
+    public void No()
+    {
+        confirm.SetActive(false);
+        exitLevel.SetActive(true);
+    }
+
+    public void Yes()
+    {
+        // Temporary Restart level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
